0df292564278bcc5458c854f787ada8b1f0edcb4
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2024-03-13 22:32:32 +0900

    [fix][add] シリアル初期化の値をdefineで指定、リーダー開始通信関数の追加

--- src/RFIDUart.hpp
+++ src/RFIDUart.hpp
@@ -2,10 +2,19 @@
 
 #include <Arduino.h>
 
+// RFIDリーダーのシリアル通信設定
+#define RFID_UART_BAUDRATE 115200
+#define RFID_UART_RX       2
+#define RFID_UART_TX       1
+
+// RFIDリーダーの動作コマンド
+#define RFID_READER_START_COMMAND "START"
+
 class RFIDUart {
 public:
     RFIDUart();
     void init();
+    void startRFIDReader();
     void update();
     void send(const char* data);
     String receive();
@@ -25,6 +34,11 @@ RFIDUart::RFIDUart() {
 
 // シリアル通信の初期化
 void RFIDUart::init() {
-    // 2ピンをRX(受信), 1ピンをTX(送信)にする
-    Serial2.begin(115200, SERIAL_8N1, 2, 1);
+    Serial2.begin(RFID_UART_BAUDRATE, SERIAL_8N1, RFID_UART_RX, RFID_UART_TX);
+}
+
+// RFIDリーダーの起動
+void RFIDUart::startRFIDReader() {
+    // RFIDリーダーの起動コマンドを送信
+    Serial2.println(RFID_READER_START_COMMAND);
 }
