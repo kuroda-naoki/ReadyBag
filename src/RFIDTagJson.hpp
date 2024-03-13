f2fd6a25cc9d47fb60b326e3cf4ace643d1bc496
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2024-03-13 18:43:50 +0900

    [add] タグIDがJsonファイル内に存在しているかを判定する関数を追加

--- src/RFIDTagJson.hpp
+++ src/RFIDTagJson.hpp
@@ -16,3 +16,52 @@ public:
     bool deleteTagFromJson(const char* tagID);
     String[] listTag();
 };
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+// ==================================================================================================================================
+// 処理の実装部分
+// ==================================================================================================================================
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+RFIDTagJson::RFIDTagJson() {
+    if (!SPIFFS.begin()) {
+        // Serial.println("SPIFFS Mount Failed");
+        return;
+    }
+}
+
+// タグIDが存在するか確認する関数
+bool RFIDTagJson::isTagIdExists(const char* tagID) {
+    // jsonファイルが存在しない場合、タグIDは存在しないと判断します。
+    if (!SPIFFS.exists(JSON_FILE)) {
+        return false;
+    }
+
+    // jsonファイルを開きます。
+    File file = SPIFFS.open(JSON_FILE, FILE_READ);
+    // ファイル読み取り失敗時
+    if (!file) {
+        return false;
+    }
+
+    // ファイルの内容をJSONオブジェクトにデシリアライズします。
+    StaticJsonDocument<1024> doc;
+    DeserializationError error = deserializeJson(doc, file);
+    if (error) {
+        // Serial.println("Failed to deserialize JSON");
+        file.close();
+        return false;
+    }
+    file.close();
+
+    // JSONオブジェクト内を検索し、タグIDが存在するかどうかを確認します。
+    for (JsonPair kv : doc.as<JsonObject>()) {
+        if (strcmp(kv.value().as<const char*>(), tagID) == 0) {
+            // 指定されたタグIDが見つかった場合
+            return true;
+        }
+    }
+
+    // 指定されたタグIDが見つからなかった場合
+    return false;
+}
