41bf4fc751cc9150d1091ac12412a4cab38ee8cd
Naoki-Kuroda-4196 <2200418@ecccomp.com>
2024-03-15 03:05:33 +0900

    [add] 設定画面の、ダイヤルがひねられたときの処理を追加

--- src/main.cpp
+++ src/main.cpp
@@ -32,17 +32,33 @@ long newPosition = 0;
 
 // かばんの中にものが存在するかどうか
 bool isExistTag = false;
+// LEDのON/OFFフラグ
+bool isLedOn = true;
+// ブザーのON/OFFフラグ
+bool isBuzzerOn = true;
 
+// メニュー画面の画像配置
 String existTagImage[3] = {PATH_MENU_GREEN_BELL, PATH_MENU_GREEN_SETTING,
                            PATH_MENU_GREEN_ADD};
 int existTagImageLength = sizeof(existTagImage) / sizeof(existTagImage[0]);
 int existTagImageIndex = 0;
-
 String notExistTagImage[3] = {PATH_MENU_RED_BELL, PATH_MENU_RED_SETTING,
                               PATH_MENU_RED_ADD};
 int notExistTagImageLength =
     sizeof(notExistTagImage) / sizeof(notExistTagImage[0]);
 int notExistTagImageIndex = 0;
+
+// 設定画面の画像配置
+String offOffImage[3] = {PATH_SETTING_TOP_OFF_OFF, PATH_SETTING_RIGHT_OFF_OFF,
+                         PATH_SETTING_LEFT_OFF_OFF};
+String onOffImage[3] = {PATH_SETTING_TOP_ON_OFF, PATH_SETTING_RIGHT_ON_OFF,
+                        PATH_SETTING_LEFT_ON_OFF};
+String offOnImage[3] = {PATH_SETTING_TOP_OFF_ON, PATH_SETTING_RIGHT_OFF_ON,
+                        PATH_SETTING_LEFT_OFF_ON};
+String onOnImage[3] = {PATH_SETTING_TOP_ON_ON, PATH_SETTING_RIGHT_ON_ON,
+                       PATH_SETTING_LEFT_ON_ON};
+int settingImageLength = sizeof(offOffImage) / sizeof(offOffImage[0]);
+int settingImageIndex = 0;
 
 // メニュー画面の選択肢
 enum Loops
@@ -129,6 +145,7 @@ void loop_menu(String tagImage[], int tagImageLength, int tagImageIndex) {
         tagImageIndex = changeImageIndex(tagImageIndex, tagImageLength,
                                          newPosition - oldPosition);
         oldPosition = newPosition;
+        // 選択肢をもとに画像の切り替え
         M5.Lcd.drawJpgFile(SPIFFS, tagImage[tagImageIndex], 0, 0);
         existTagImageIndex = tagImageIndex;
         notExistTagImageIndex = tagImageIndex;
@@ -146,6 +163,55 @@ void loop_menu(String tagImage[], int tagImageLength, int tagImageIndex) {
             case 2:
                 currentLoops = ADDTAG;
                 break;
+        }
+    }
+}
+
+// 設定画面のループ関数
+void loop_setting() {
+    newPosition = M5Dial.Encoder.read();
+
+    // ダイヤルがひねられたときの処理
+    if (newPosition != oldPosition) {
+        M5Dial.Speaker.tone(8000, 20);
+        settingImageIndex = changeImageIndex(
+            settingImageIndex, settingImageLength, newPosition - oldPosition);
+        oldPosition = newPosition;
+        // 選択肢とブザー、LEDのON/OFFもとに画像の切り替え
+        changeOnOffImage(settingImageIndex);
+    }
+
+    // ボタンが押されたときの処理
+    if (M5Dial.BtnA.wasPressed()) {
+        switch (settingImageIndex) {
+            case 0:
+                currentLoops = MENU;
+                break;
+            case 1:
+                isBuzzerOn = !isBuzzerOn;
+                changeOnOffImage(settingImageIndex);
+                break;
+            case 2:
+                isLedOn = !isLedOn;
+                changeOnOffImage(settingImageIndex);
+                break;
+        }
+    }
+}
+
+// 設定画面の画像を切り替える関数
+void changeOnOffImage(int index) {
+    if (isLedOn) {
+        if (isBuzzerOn) {
+            M5.Lcd.drawJpgFile(SPIFFS, onOnImage[index], 0, 0);
+        } else {
+            M5.Lcd.drawJpgFile(SPIFFS, onOffImage[index], 0, 0);
+        }
+    } else {
+        if (isBuzzerOn) {
+            M5.Lcd.drawJpgFile(SPIFFS, offOnImage[index], 0, 0);
+        } else {
+            M5.Lcd.drawJpgFile(SPIFFS, offOffImage[index], 0, 0);
         }
     }
 }
